6315e85c9c3a65178c20a93cdf7cca00a4e9f585
jeanpierre89 <1jeanpierre.03@gmail.com>
2024-06-09 10:52:22 -0300

    ajuste de texto

--- resources/views/layout/abaut.blade.php
+++ resources/views/layout/abaut.blade.php
@@ -2,46 +2,46 @@
     <div class="max-w-6xl mx-auto font-[sans-serif] text-gray-800 bg-gray-100 px-6 py-12">
         <div class="grid lg:grid-cols-2 gap-8 max-lg:max-w-2xl mx-auto">
           <div class="text-left">
-            <h2 class="text-4xl font-extrabold mb-6">Welcome to Our Website</h2>
-            <p class="mb-4 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquam, ipsum vel iaculis bibendum, justo turpis ullamcorper mauris, non aliquam nisi purus vel nisl. Integer efficitur turpis in bibendum tincidunt.</p>
-            <p class="mb-4 text-sm">Nulla facilisi. Vestibulum fringilla leo et purus consectetur, vel tincidunt dolor rhoncus. In hac habitasse platea dictumst. Fusce vel sodales elit. Suspendisse potenti. Sed eget consequat nisi.</p>
-            <p class="text-sm">consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula. nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+            <h2 class="text-4xl font-extrabold mb-6">Olá Me chamo Jean Pierre </h2>
+            <p class="mb-4 text-sm">Com experiência em diversass áreas onde já atuei como liderança e operacional, home office e presencialmente.</p>
+            <p class="mb-4 text-sm">Todas oportunidades que tive atuei de forma onesta e profissional, sempre buscando atender as expectativas minhas e dos meus liderandos .</p>
+            <p class="text-sm">Busquei criar relações profissionai respeitosas com qualidade nos minhas atividades de forma que me destacava na maioria das vezes.</p>
             <div class="grid xl:grid-cols-3 sm:grid-cols-2 gap-8 mt-12">
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Customization</h6>
+                <h6 class="text-base font-semibold ml-4">Proatividade</h6>
               </div>
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Security</h6>
+                <h6 class="text-base font-semibold ml-4">Auto crescimento</h6>
               </div>
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Support</h6>
+                <h6 class="text-base font-semibold ml-4">Senço de equipe</h6>
               </div>
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Performance</h6>
+                <h6 class="text-base font-semibold ml-4">Pericia </h6>
               </div>
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Global Reach</h6>
+                <h6 class="text-base font-semibold ml-4">Inovação</h6>
               </div>
               <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="fill-green-500 shrink-0" viewBox="0 0 24 24">
                   <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
                 </svg>
-                <h6 class="text-base font-semibold ml-4">Communication</h6>
+                <h6 class="text-base font-semibold ml-4">Comunicativo</h6>
               </div>
             </div>
           </div>
